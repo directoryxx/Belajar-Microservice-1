c97644d6b43aeac51b991265a91ba44b964d9eee
Anak Agung Angga Wijaya <anggawijaya4567@gmail.com>
2020-09-26 23:28:22 +0700

    comment service & init front

---
+++ comments-service/index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const {randomBytes} = require('crypto');
+
+
+const app = express();
+app.use(bodyParser.json());
+
+const commentByPostById = {};
+
+app.get('/posts/:id/comments', (req, res) => {
+    res.send(commentByPostById[req.params.id] || []);
+});
+
+app.post('/posts/:id/comments', (req, res) => {
+    const commentId = randomBytes(4).toString('hex');
+    const { content } = req.body;
+
+    const comments = commentByPostById[req.params.id] || [];
+    comments.push({ id: commentId, content });
+
+    commentByPostById[req.params.id] = comments;
+
+    res.status(200).send(comments);
+});
+
+app.listen('4001', () => {
+    console.log("Listening on port 4001 - Commments Service")
+});
